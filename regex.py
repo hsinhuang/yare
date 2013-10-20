873dd1b8262767c35c84b8108db549ded1d2114e
leon-huang <leon@njuopen.com>
2013-10-20 17:04:11 +0800

    parser not completed: need refactor

--- regex.py
+++ regex.py
@@ -3,10 +3,14 @@
 
 """init file of the package"""
 
-from pyre.fa import FA, EPSILON
+from fa import FA, EPSILON
 
 ESCAPE = '\\'
 SPECIAL_SYMBOLS = '\\e|*()'
+
+SELECT = 'selection'
+CONCAT = 'concatenation'
+LOOP   = 'loop'
 
 class RegEx:
     """Regular Expression based on minimal DFA"""
@@ -52,6 +56,84 @@ def __split(pattern):
             else:
                 tokens.append(char)
     return tokens
+
+__matched_rparen = {}
+
+def __seeking_rparen(tokens, start_idx):
+    """
+    return the index of matched right parenthesis in tokens
+    of the left parenthesis in `start_idx`
+    """
+    assert tokens[start_idx] == '('
+    tokens_str = ''.join(tokens)
+    if tokens_str not in __matched_rparen:
+        __matched_rparen.setdefault(tokens_str, {})
+    if start_idx not in __matched_rparen[tokens_str]:
+        __matched_rparen[tokens_str][start_idx] = \
+            __seeking_rparen_noncached(tokens, start_idx)
+    return __matched_rparen[tokens_str][start_idx]
+
+def __seeking_rparen_noncached(tokens, start_idx):
+    """
+    return the index of matched right parenthesis in tokens
+    of the left parenthesis in `start_idx`
+
+    this version do not cache
+    """
+    assert tokens[start_idx] == '('
+    start_idx += 1
+    while start_idx < len(tokens):
+        current = tokens[start_idx]
+        if current == ')':
+            return start_idx
+        if current == '(':
+            start_idx = __seeking_rparen(tokens, start_idx)
+        start_idx += 1
+    return None
+
+def __seeking_vertical_bar(tokens, start_idx):
+    """
+    return the index of fisrt vertical bar in tokens,
+    or None if there is a '(' or ')' before it
+    """
+    while start_idx < len(tokens):
+        current = tokens[start_idx]
+        if current == '|':
+            return start_idx
+        elif current in '()':
+            return None
+        start_idx += 1
+    return None
+
+def __parser(tokens):
+    """parse a pattern string to a syntax tree"""
+    if not tokens:
+        return None
+    num_tokens = len(tokens)
+    selection = []
+    index = 0
+    while index < num_tokens:
+        current = tokens[index]
+        if current == '(':
+            rparen_idx = __seeking_rparen(tokens, index)
+            if not rparen_idx:
+                raise SyntaxError("Parenthesis not matched, ", index)
+            if rparen_idx+1 < num_tokens and tokens[rparen_idx+1] == '*':
+                rparen_idx += 1
+            selection.append(tokens[index:rparen_idx+1])
+            index = rparen_idx+1
+        elif current == ')':
+            raise SyntaxError("Parenthesis redundant, ", index)
+        elif current == '|':
+            raise SyntaxError("Vertical bar not valid, ", index)
+        elif current == '*':
+            raise SyntaxError("Star not valid, ", index)
+        elif current == '\\':
+            raise SyntaxError("Backslash not valid, ", index)
+        else:
+            bar_idx = __seeking_vertical_bar(tokens, index)
+            if not bar_idx:
+
 
 def __grow_nfa(nfa, tokens, start_index=0):
     """
