bac2c846921604119ab6c321ee846ec487c485c2
leon-huang <leon@njuopen.com>
2013-10-20 14:36:50 +0800

    make instace variables protected

--- fa.py
+++ fa.py
@@ -5,43 +5,43 @@
 
 EPSILON = r'\0'
 
-class FA:
+class FA: #pylint: disable=W0212
     """
     Finite Automata
 
     EPSILON indicates epsilon
     """
     def __init__(self):
-        self.acceptable = set()
-        self.nodes = set()
-        self.map = {}
-        self.start = None
-        self.finals = set()
-        self.epsilon_clos = {}
+        self.__acceptable = set()
+        self.__nodes = set()
+        self.__map = {}
+        self.__start = None
+        self.__finals = set()
+        self.__epsilon_clos = {}
 
     def relabel(self):
         """relabel the finite automata"""
         new = FA()
-        relabel_map = dict(zip(self.nodes.difference({ self.start }),
-                               range(1, len(self.nodes))))
-        relabel_map[self.start] = 0
-        new.acceptable = self.acceptable.copy()
-        new.nodes = { relabel_map[old] for old in self.nodes }
-        new.map = { relabel_map[old]:
+        relabel_map = dict(zip(self.__nodes.difference({ self.__start }),
+                               range(1, len(self.__nodes))))
+        relabel_map[self.__start] = 0
+        new.__acceptable = self.__acceptable.copy()
+        new.__nodes = { relabel_map[old] for old in self.__nodes }
+        new.__map = { relabel_map[old]:
                         { edge: { relabel_map[node]
-                                  for node in self.map[old][edge]
+                                  for node in self.__map[old][edge]
                                 }
-                          for edge in self.map[old]
+                          for edge in self.__map[old]
                         }
-                    for old in self.map
+                    for old in self.__map
                   }
-        new.start = relabel_map[self.start]
-        new.finals = { relabel_map[old] for old in self.finals }
-        self.epsilon_clos = { relabel_map[old]:
+        new.__start = relabel_map[self.__start]
+        new.__finals = { relabel_map[old] for old in self.__finals }
+        self.__epsilon_clos = { relabel_map[old]:
                                 { relabel_map[e]
-                                  for e in self.epsilon_clos[old]
+                                  for e in self.__epsilon_clos[old]
                                 }
-                              for old in self.epsilon_clos
+                              for old in self.__epsilon_clos
                             }
         return new
 
@@ -49,20 +49,20 @@ class FA:
         """deep copy"""
         new = FA()
         from copy import deepcopy
-        new.acceptable = self.acceptable.copy()
-        new.nodes = self.nodes.copy()
-        new.map = deepcopy(self.map)
-        new.start = self.start
-        new.finals = self.finals.copy()
-        self.epsilon_clos = deepcopy(self.epsilon_clos)
+        new.__acceptable = self.__acceptable.copy()
+        new.__nodes = self.__nodes.copy()
+        new.__map = deepcopy(self.__map)
+        new.__start = self.__start
+        new.__finals = self.__finals.copy()
+        self.__epsilon_clos = deepcopy(self.__epsilon_clos)
         return new
 
     def partition(self, group, groups):
         """try to partition group"""
-        for edge in self.acceptable:
+        for edge in self.__acceptable:
             parti = {}
             for node in group:
-                idx = [ list(self.map[node][edge])[0] in g for g in groups ]\
+                idx = [ list(self.__map[node][edge])[0] in g for g in groups ]\
                     .index(True)
                 parti.setdefault(idx, set())
                 parti[idx].add(node)
@@ -73,7 +73,7 @@ class FA:
     def minimize(self):
         """minimize the number of states of the DFA"""
         assert self.is_dfa()
-        groups = [self.finals, self.nodes.difference(self.finals)]
+        groups = [self.__finals, self.__nodes.difference(self.__finals)]
         from copy import deepcopy
         new_groups = deepcopy(groups)
         parti = True
@@ -93,12 +93,12 @@ class FA:
         for group in groups:
             for node in group:
                 divide[node] = tuple(group)
-        for start in self.map:
-            for edge in self.map[start]:
-                for dst in self.map[start][edge]:
+        for start in self.__map:
+            for edge in self.__map[start]:
+                for dst in self.__map[start][edge]:
                     new.connect(divide[start], divide[dst], edge)
-        new.set_start(divide[self.start])
-        _ = [new.add_final(divide[f]) for f in self.finals]
+        new.set_start(divide[self.__start])
+        _ = [new.add_final(divide[f]) for f in self.__finals]
         return new
 
     def reachable(self, nodes, edge):
@@ -106,14 +106,14 @@ class FA:
         return all the reachable nodes from any of the node in
         `nodes` via `edge`
         """
-        if edge not in self.acceptable:
+        if edge not in self.__acceptable:
             return None
         result = set()
         key = set()
         for node in nodes:
             for new_node in self.epsilon_closure(node):
-                if new_node in self.map and edge in self.map[new_node]:
-                    for next_node in self.map[new_node][edge]:
+                if new_node in self.__map and edge in self.__map[new_node]:
+                    for next_node in self.__map[new_node][edge]:
                         key.add(next_node)
                         result.update(self.epsilon_closure(next_node))
         return (key, result)
@@ -121,10 +121,10 @@ class FA:
     def make_dfa(self):
         """return a NFA corresponding to DFA"""
         new = FA()
-        valid_acceptable = self.acceptable.copy()
+        valid_acceptable = self.__acceptable.copy()
         valid_acceptable.remove(EPSILON)
-        new_nodes_set = { (self.start,) }
-        nodes_unmarked = { (self.start,) }
+        new_nodes_set = { (self.__start,) }
+        nodes_unmarked = { (self.__start,) }
 
         while nodes_unmarked:
             node = nodes_unmarked.pop()
@@ -134,20 +134,20 @@ class FA:
                     new_nodes_set.add(tuple(key))
                     nodes_unmarked.add(tuple(key))
                 new.connect(node, tuple(key), edge)
-                for final in self.finals:
+                for final in self.__finals:
                     if final in e_clos:
                         new.add_final(tuple(key))
 
-        new.set_start((self.start,))
+        new.set_start((self.__start,))
         return new
 
     def is_dfa(self):
         """whether the FA is deterministic """
-        if EPSILON in self.acceptable:
+        if EPSILON in self.__acceptable:
             return False
-        for node in self.map:
-            for edge in self.map[node]:
-                if len(self.map[node][edge]) > 1:
+        for node in self.__map:
+            for edge in self.__map[node]:
+                if len(self.__map[node][edge]) > 1:
                     return False
         return True
 
@@ -155,38 +155,38 @@ class FA:
         """
         >>> fa = FA()
         >>> fa.connect(0, 1, 'a')
-        self.map = { 0: { 'a': {1} } }
+        self.__map = { 0: { 'a': {1} } }
         >>> fa.connect(0, 2, 'a').connect(1, 2, 'b')
-        self.map = { 0: { 'a': {1, 2} },
+        self.__map = { 0: { 'a': {1, 2} },
                      1: { 'b': {2} } }
         """
-        self.map.setdefault(from_node, {})
-        self.map[from_node].setdefault(edge, set())
-        self.map[from_node][edge].add(to_node)
-        self.acceptable.add(edge)
-        self.nodes.add(from_node)
-        self.nodes.add(to_node)
+        self.__map.setdefault(from_node, {})
+        self.__map[from_node].setdefault(edge, set())
+        self.__map[from_node][edge].add(to_node)
+        self.__acceptable.add(edge)
+        self.__nodes.add(from_node)
+        self.__nodes.add(to_node)
         return self
 
     def epsilon_closure(self, node):
         """
         return the epsilon closure of the given node
         """
-        if node not in self.epsilon_clos:
-            if node not in self.nodes:
+        if node not in self.__epsilon_clos:
+            if node not in self.__nodes:
                 return None
             closure = set()
-            if node in self.map and EPSILON in self.map[node]:
-                closure.update(self.map[node][EPSILON])
+            if node in self.__map and EPSILON in self.__map[node]:
+                closure.update(self.__map[node][EPSILON])
             new_closure = closure.copy()
             if node in new_closure:
                 new_closure.remove(node)
             for new_node in closure:
-                if self.epsilon_closure(new_node):
-                    new_closure.update(self.epsilon_closure(new_node))
+                if self.__epsilon_closure(new_node):
+                    new_closure.update(self.__epsilon_closure(new_node))
             new_closure.add(node)
-            self.epsilon_clos[node] = new_closure
-        return self.epsilon_clos[node]
+            self.__epsilon_clos[node] = new_closure
+        return self.__epsilon_clos[node]
 
     def validate(self, edges):
         """
@@ -198,40 +198,40 @@ class FA:
         return the reachable finals when there are some,
         otherwise return an empty list
         """
-        current = self.epsilon_closure(self.start)
+        current = self.__epsilon_closure(self.__start)
         for edge in edges:
             new_current = set()
             for node in current:
-                if node in self.map and edge in self.map[node]:
-                    for next_node in self.map[node][edge]:
-                        new_current.update(self.epsilon_closure(next_node))
+                if node in self.__map and edge in self.__map[node]:
+                    for next_node in self.__map[node][edge]:
+                        new_current.update(self.__epsilon_closure(next_node))
             current = new_current
         terminants = []
         for node in current:
-            if node in self.finals:
+            if node in self.__finals:
                 terminants.append(node)
         return terminants
 
     def start_node(self):
         """getter: start node"""
-        return self.start
+        return self.__start
 
     def set_start(self, node):
         """setter: start node"""
-        if node in self.map:
-            self.start = node
+        if node in self.__map:
+            self.__start = node
         else:
             return None
         return self
 
     def final_nodes(self):
         """getter: final nodes"""
-        return self.finals
+        return self.__finals
 
     def add_final(self, node):
         """setter: final nodes"""
-        if node in self.nodes:
-            self.finals.add(node)
+        if node in self.__nodes:
+            self.__finals.add(node)
         else:
             return None
         return self
