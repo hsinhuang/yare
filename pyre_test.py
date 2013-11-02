3c48c1121e6d1ca7639cf073722ce85a519bfe57
leon-huang <leon@njuopen.com>
2013-11-02 14:28:27 +0800

    test framework in detail

--- pyre_test.py
+++ pyre_test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding:utf-8
+#pylint: disable=C0103
+#pylint: disable=R0904
 
 """unit test cases for the package"""
 
@@ -7,8 +9,47 @@ import random
 import unittest
 import pyre
 
-class TestPyre(unittest.TestCase):
-    pass
+class TestCaseSimpleSelection(unittest.TestCase):
+    """simple test case: selection"""
+    def setUp(self):
+        """compile a regex"""
+        self.__regex__ = pyre.compile(r'a|b')
+    def test_match(self):
+        """test method `match`"""
+        positive = []
+        negative = []
+        for case in positive:
+            self.assertTrue(self.__regex__.match(case))
+        for case in negative:
+            self.assertFalse(self.__regex__.match(case))
+
+class TestCaseSimpleConcatenation(unittest.TestCase):
+    """simple test case: concatenation"""
+    def setUp(self):
+        """compile a regex"""
+        self.__regex__ = pyre.compile(r'ab')
+    def test_match(self):
+        """test method `match`"""
+        positive = []
+        negative = []
+        for case in positive:
+            self.assertTrue(self.__regex__.match(case))
+        for case in negative:
+            self.assertFalse(self.__regex__.match(case))
+
+class TestCaseSimpleLoop(unittest.TestCase):
+    """simple test case: loop"""
+    def setUp(self):
+        """compile a regex"""
+        self.__regex__ = pyre.compile(r'a*')
+    def test_match(self):
+        """test method `match`"""
+        positive = []
+        negative = []
+        for case in positive:
+            self.assertTrue(self.__regex__.match(case))
+        for case in negative:
+            self.assertFalse(self.__regex__.match(case))
 
 if __name__ == '__main__':
     unittest.main()
