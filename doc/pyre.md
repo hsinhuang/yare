767956e92b4eab49a8d7874d09e184869194d4f7
leon-huang <leon@njuopen.com>
2013-11-02 17:00:42 +0800

    an installable package

---
+++ doc/pyre.md
@@ -0,0 +1,25 @@
+Supported Syntax
+----
+
+In total, syntax supported by the library is the most basic ones.
+As following described in detail:
+
+1. selection: `a|b`
+
+2. concatenation: `ab`
+
+3. loop: `a*`
+
+
+Special Symbol
+----
+
+0. `\`: escape. To indicate backslash itself, use `\\` instead.
+
+1. `\e`: epsilon.
+
+2. `|`: selection. To indicate vertical bar, use `\|` instead.
+
+3. `*`: loop. To indicate star, use `\*` instead.
+
+4. `(` and `)`: group. To indicate parentheses, use `\(` and `\)` instead.
---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name = "pyre",
+    description="Python Regular Expression",
+    long_description = """
+PYRE is a simple regular expression library implemented in Python.
+It is fit for teaching and learning.
+It is compatible with both Python 2 and Python 3.
+""",
+    license="""GPL v2""",
+    version = "0.2",
+    author = "Leonardo Huang",
+    author_email = "leon@njuopen",
+    maintainer = "Leonardo Huang",
+    maintainer_email = "leon@njuopen",
+    url = "https://github.com/leon-huang/pyre",
+    packages = ['pyre'],
+    install_requires = ['ply'],
+    classifiers = [
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 2',
+    ]
+)
