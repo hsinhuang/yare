3c436a0d889e173931e460bee60d882cd043574b
leon-huang <leon@njuopen.com>
2013-11-12 22:10:53 +0800

    v0.5

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ It is fit for teaching and learning.
 It is compatible with both Python 2 and Python 3.
 """,
     license="""GPL v2""",
-    version = "0.4",
+    version = "0.5",
     author = "Leonardo Huang",
     author_email = "leon@njuopen",
     maintainer = "Leonardo Huang",
     maintainer_email = "leon@njuopen",
     url = "https://github.com/leon-huang/yare",
     packages = ['yare'],
-    install_requires = ['ply'],
+    install_requires = [],
     classifiers = [
       'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 2',
